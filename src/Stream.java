aa0b39e716cb868a61aa91026ee7d50c0cac2130
itayventura <ventura.itay@gmail.com>
2021-02-11 21:51:20 +0200

    adding a method using filter method for strings

--- src/Stream.java
+++ src/Stream.java
@@ -15,11 +15,19 @@ public class Stream {
         return list.stream().filter(isEven).collect(Collectors.toList());
     }
 
+    public static List<String> longerThanFiveChars(List<String> list){
+        Predicate<String> isLongerThanFiveChar = s -> s.length() > 5;
+        return list.stream().filter(isLongerThanFiveChar).collect(Collectors.toList());
+    }
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4));
         System.out.println("before doubling " + (list));
         System.out.println("after doubling " + doubled(list));
         System.out.println("after isEven" + evens(list));
+
+        List<String> words = new ArrayList<>(Arrays.asList("a", "ab", "abc", "abcd", "abcde", "abcdef"," abcdefg"));
+        System.out.println("before filter " + words);
+        System.out.println("after filter " + longerThanFiveChars(words));
 
     }
 }

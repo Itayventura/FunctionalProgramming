563a5d9a99aea09a3c9f8d146c891d60049943b7
itayventura <ventura.itay@gmail.com>
2021-02-11 22:11:27 +0200

    adding sum method using reduce

--- src/Stream.java
+++ src/Stream.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -21,6 +22,16 @@ public class Stream {
         Predicate<String> isLongerThan = predicateGenerator.apply(numOfChars);
         return list.stream().filter(isLongerThan).collect(Collectors.toList());
     }
+
+    public static int sum(List<Integer> list){
+
+        BinaryOperator<Integer> calcSum = (acc, x) -> acc + x;
+        BinaryOperator<Integer> calcSumSimplified = Integer::sum;
+        return list.stream().reduce(0, calcSum);
+
+
+
+    }
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4));
         System.out.println("before doubling " + (list));
@@ -30,6 +41,8 @@ public class Stream {
         List<String> words = new ArrayList<>(Arrays.asList("a", "ab", "abc", "abcd", "abcde", "abcdef"," abcdefg"));
         System.out.println("before filter with 5 " + words);
         System.out.println("after filter with 5 " + longerThan(5, words));
+
+        System.out.println("sum: " + sum(list));
 
     }
 }

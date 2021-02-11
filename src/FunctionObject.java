8f70e7e519fb08f0e8fabd5f4808445a3a3533af
itayventura <ventura.itay@gmail.com>
2021-02-11 20:45:54 +0200

    adding the option of passing a function as an argument

--- src/FunctionObject.java
+++ src/FunctionObject.java
@@ -1,3 +1,4 @@
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class FunctionObject {
@@ -6,10 +7,19 @@ public class FunctionObject {
         public static Integer triple(Integer x){
             return x*3;
         }
+        public static Integer subtract(Integer x, Integer y){ return x-y;}
+        public static Integer add(Integer x, Integer y){ return x+y;}
+        public static Integer combineTwoAndThree(BiFunction<Integer, Integer, Integer> function){
+            return function.apply(2,3);
+        }
+
     }
 
     public static void main(String[] args){
         Function<Integer, Integer> myTriple = MyMath::triple;
         System.out.println(myTriple.apply(5));
+
+        System.out.println(MyMath.combineTwoAndThree(MyMath::add));
+        System.out.println(MyMath.combineTwoAndThree(MyMath::subtract));
     }
 }
